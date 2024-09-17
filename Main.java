f7f9aeb80c77e9dd78cb1321280819666b98ec53
Ryan Liu <ryanliu12620@gmail.com>
2024-09-17 15:13:29 -0400

    -Fixed files -Completed decimal formating -Made the reciept format look better with boxes and stuff -Added more coments patch

--- Main.java
+++ Main.java
@@ -7,23 +7,20 @@ class Main
     public static void main(String[] args)
     {
         // Decimal formating for money values
+        // # means any non-zero is placeholder, 0 means any number is a placeholder
         DecimalFormat dc1 = new DecimalFormat("###.00");
 
-
-
-        // Start with the story
-        // Init variables that look nice
-
+        // Creates date for reciept
         Date currentDate = new Date();
-        System.out.println("Current date and time: " + currentDate);
 
+        // Init variables that make reciept look fancy
         String restaurantName = "Hon Hon Hon Fancy Dining";
         final String ADDRESS = "7818 Extortion Avenue";
         final String RESTURANT_NUMBER = "929-311-4592";
         final String MERCHANT_ID = "HIGH_1827Hb";
         final String ENDING_SIGN = "Thanks for supporting us!\nTHANK YOU";
         String status = "Approved";
-        String seperation1 = "----------------------------";
+        String seperation1 = "-------------------------------";
 
         // Init variables for caluulations
         int numberOfPeople;
@@ -34,8 +31,9 @@ class Main
         double billPerPerson;
         double tipPerPerson;
 
-        // Ask for input information
-        // Scanner
+        // Start with the story
+        // Ask for input with Scanner
+        // Inputs are for money and tip 
         Scanner s = new Scanner(System.in);
 
         System.out.print("Enter your Bill (S): ");
@@ -50,38 +48,38 @@ class Main
         String tempNumberOfPeople = s.nextLine();
         numberOfPeople = Integer.parseInt(tempNumberOfPeople);
 
+        // CLose scanner
+        s.close();
         
         // Calculations
-        totalTip = tipPercentage * subTotal;
-        finalBill = totalTip + subTotal;
-        tipPerPerson = totalTip / numberOfPeople;
-        billPerPerson = finalBill / numberOfPeople;
+        // Format all numbers with dc1
+        totalTip = Double.parseDouble(dc1.format(tipPercentage * subTotal));
+        finalBill = Double.parseDouble(dc1.format(totalTip + subTotal));
+        tipPerPerson = Double.parseDouble(dc1.format(totalTip / numberOfPeople));
+        billPerPerson = Double.parseDouble(dc1.format(finalBill / numberOfPeople));
 
 
-        System.out.println(restaurantName);
-        System.out.println(ADDRESS);
-        System.out.println(RESTURANT_NUMBER);
+        // Output results
+        // Results are boxed
         System.out.println();
-        System.out.println(currentDate);
-        System.out.println("Merchant ID: " + MERCHANT_ID);
-        System.out.println("Response: " + status);
-        System.out.println("Sub Total: USD$ " + subTotal);
-        System.out.println("Tip: USD$ " + totalTip);
+        System.out.println(seperation1);
+        System.out.println("|" + restaurantName);
+        System.out.println("|" + ADDRESS);
+        System.out.println("|" + RESTURANT_NUMBER);
+        System.out.println(seperation1);
+        System.out.println("|" + currentDate);
+        System.out.println("|" + "Merchant ID: " + MERCHANT_ID);
+        System.out.println("|" + "Response: " + status);
+        System.out.println("|" + "Sub Total: USD$ " + subTotal);
+        System.out.println("|" + "Tip: USD$ " + totalTip);
 
         System.out.println(seperation1);
-        System.out.println("Total bill: USD$ " + finalBill);
-        System.out.println("Tip per person: USD$ " + tipPerPerson);
-        System.out.println("Bill per person: USD$ " + billPerPerson);
-
-
-        System.out.println();
+        System.out.println("|" + "Total bill: USD$ " + finalBill);
+        System.out.println("|" + "Tip per person: USD$ " + tipPerPerson);
+        System.out.println("|" + "Bill per person: USD$ " + billPerPerson);
+        System.out.println(seperation1);
         System.out.println();
         System.out.println(ENDING_SIGN);
-
-
-
-
-
 
     }
 }
